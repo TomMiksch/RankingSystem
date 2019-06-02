fcedec156bfbcf56abf5766869415cd2bff5371a
TomM <thomas-miksch@uiowa.edu>
2019-06-01 21:09:32 -0500

    Sorts by total wins

--- RankingSystem.py
+++ RankingSystem.py
@@ -1,9 +1,23 @@
 from sportsreference.ncaaf.boxscore import Boxscore
 from sportsreference.ncaaf.boxscore import Boxscores
+from sportsreference.ncaaf.teams import Team
+from sportsreference.ncaaf.teams import Teams
 from datetime import datetime
+import time
+import collections
 
 # this is all garbage and doesn't do anything yet
-games = Boxscores(datetime(2017, 11, 4))
-game = Boxscore("2017-11-04-iowa")
-print("Winners on 11 November 2017")
-print(game.winning_name)
+start = time.time()
+
+teamRankOrder = collections.OrderedDict()
+
+teams = Teams()
+for team in teams:
+    teamRankOrder[team.name] = team.wins
+
+for key, value in sorted(teamRankOrder.items(), key=lambda item: item[1], reverse = True):
+    print("%s: %s" % (key, value))
+
+stop = time.time()
+
+print("took " + str(stop - start) + " seconds")
